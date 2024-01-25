471ed0a480b6e4743e49096e36c88261a6a1c8d7
Nikita Volobuev <nikitavbv@gmail.com>
2024-01-25 23:21:53 +0200

    feat(server): add axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    tracing::{Level, info},
+    tracing_subscriber::FmtSubscriber,
+    tokio::net::TcpListener,
+    axum::{Router, routing::get},
+};
+
+#[tokio::main]
+async fn main() {
+    init_logging();
+
+    let addr = "0.0.0.0:3000";
+    info!("starting llama inference server on {}", addr);
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+fn init_logging() {
+    FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .init();
 }
