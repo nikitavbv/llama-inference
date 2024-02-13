7f5199caa3253b522359caf086fd1e3d7fcb7679
Nikita Volobuev <nikitavbv@gmail.com>
2024-02-13 22:28:52 +0200

    feat(server): run model as blocking task

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use {
+    std::sync::Mutex,
     crate::model::ChatModel,
     async_openai::types::{ChatChoice, ChatCompletionResponseMessage, CreateChatCompletionResponse, Role},
     axum::{
@@ -13,7 +14,7 @@ use {
     rand::{distributions::Alphanumeric, Rng},
     serde::{Deserialize, Serialize},
     std::{sync::Arc, time::{SystemTime, UNIX_EPOCH}},
-    tokio::{net::TcpListener, sync::Mutex},
+    tokio::net::TcpListener,
     tracing::{info, warn, Level},
     tracing_subscriber::FmtSubscriber,
 };
@@ -71,21 +72,24 @@ async fn chat_completions(
     State(state): State<AppState>,
     Json(request): Json<ChatCompletionsRequest>,
 ) -> Json<CreateChatCompletionResponse> {
-    let mut model = state.model.lock().await;
-    let completion = model.chat_completions(
-        request
-            .messages
-            .into_iter()
-            .map(|message| model::ChatMessage {
-                role: match message.role {
-                    ChatRole::System => model::ChatRole::System,
-                    ChatRole::User => model::ChatRole::User,
-                    ChatRole::Assistant => model::ChatRole::Assistant,
-                },
-                content: message.content,
-            })
-            .collect(),
-    );
+    let completion = tokio::task::spawn_blocking(move || {
+        let mut model = state.model.lock().unwrap();
+
+        model.chat_completions(
+            request
+                .messages
+                .into_iter()
+                .map(|message| model::ChatMessage {
+                    role: match message.role {
+                        ChatRole::System => model::ChatRole::System,
+                        ChatRole::User => model::ChatRole::User,
+                        ChatRole::Assistant => model::ChatRole::Assistant,
+                    },
+                    content: message.content,
+                })
+                .collect(),
+        )
+    }).await.unwrap();
 
     Json(CreateChatCompletionResponse {
         id: response_id(),
